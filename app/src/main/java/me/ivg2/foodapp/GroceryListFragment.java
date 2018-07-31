46280a14b66614a2de6f5d33bda34e7ff533d9b7
Isabel Greiner <ivg2@fb.com>
2018-07-31 10:21:50 -0700

    Set up basic grocery list for MVP.

---
+++ app/src/main/java/me/ivg2/foodapp/GroceryListFragment.java
@@ -0,0 +1,54 @@
+package me.ivg2.foodapp;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class GroceryListFragment extends Fragment {
+
+    private GroceryListAdapter adapter;
+    @BindView(R.id.rvGrocery)
+    RecyclerView rvGroceries;
+    private Unbinder unbinder;
+
+    public GroceryListFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_grocery_list, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        unbinder = ButterKnife.bind(this, view);
+        adapter = new GroceryListAdapter(GroceryListItemRepository.getInstance());
+        rvGroceries.setLayoutManager(new LinearLayoutManager(getActivity()));
+        rvGroceries.setAdapter(adapter);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+}
