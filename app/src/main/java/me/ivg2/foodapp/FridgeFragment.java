c8d55c7a9c73aad48be030328042c4c6d8020222
Isabel Greiner <ivg2@fb.com>
2018-08-10 13:33:24 -0700

    Font changes and fixed bug for missing ingredients.

--- app/src/main/java/me/ivg2/foodapp/FridgeFragment.java
+++ app/src/main/java/me/ivg2/foodapp/FridgeFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
@@ -61,6 +62,7 @@ public class FridgeFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle("My Fridge");
         unbinder = ButterKnife.bind(this, view);
         foods = FoodItemRepository.getInstance();
         fridgeSearchBar = view.findViewById(R.id.searchBarFridge);
