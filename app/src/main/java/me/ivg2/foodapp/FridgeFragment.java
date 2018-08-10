50a4b5400bd27c481cf7b3280b47c1252ad2799b
Gina Valderrama <ginav@fb.com>
2018-08-10 15:13:35 -0700

    actionBar crash

--- app/src/main/java/me/ivg2/foodapp/FridgeFragment.java
+++ app/src/main/java/me/ivg2/foodapp/FridgeFragment.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
@@ -62,7 +61,6 @@ public class FridgeFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle("My Fridge");
         unbinder = ButterKnife.bind(this, view);
         foods = FoodItemRepository.getInstance();
         fridgeSearchBar = view.findViewById(R.id.searchBarFridge);
